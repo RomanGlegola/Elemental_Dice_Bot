3d206ccf2898af48028cc98632bcdb150076abf4
Roman Glegoła <romanglegola@gmail.com>
2023-08-15 07:13:28 +0200

    main: add notations and fix docstrings
    Signed-off-by: Roman Glegoła <romanglegola@gmail.com>

--- requirements.py
+++ requirements.py
@@ -2,9 +2,21 @@ import subprocess
 
 
 class PipInstaller:
+    """
+    A class to facilitate the installation of Python packages using pip.
 
+    Methods:
+        install_from_requirements(file_path="requirements.txt"):
+            Installs packages listed in a requirements file.
+    """
     @staticmethod
-    def install_from_requirements(file_path="requirements.txt"):
+    def install_from_requirements(file_path: str = "requirements.txt"):
+        """
+        Install packages listed in a requirements file using pip.
+
+        Args:
+            file_path (str): Path to the requirements file (default is "requirements.txt").
+        """
         with open(file_path, 'r') as f:
             packages = f.readlines()
         for package in packages:
